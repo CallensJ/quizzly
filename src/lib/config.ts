f158e4743cfd443869316fc5378dab71a4733253
CallensJ <callens.johan@protonmail.com>
2026-04-18 14:35:07 +0800

    refactor auth.ts

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_APP_URL ||
+  process.env.NEXT_PUBLIC_SITE_URL ||
+  (typeof window !== "undefined"
+    ? window.location.origin
+    : "http://localhost:3000");
